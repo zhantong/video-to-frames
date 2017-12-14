620d464abefa0aaad6bba61d961359d6a7308491
Tong ZHAN <zhantong1994@163.com>
2017-12-14 14:38:30 +0800

    增加保存为I420/NV21格式帧功能

--- videotoframes/src/main/java/com/polarxiong/videotoframes/VideoToFrames.java
+++ videotoframes/src/main/java/com/polarxiong/videotoframes/VideoToFrames.java
@@ -1,5 +1,7 @@
 package com.polarxiong.videotoframes;
 
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
 import android.media.Image;
 import android.media.MediaCodec;
 import android.media.MediaCodecInfo;
@@ -8,6 +10,7 @@ import android.media.MediaFormat;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -19,6 +22,10 @@ import java.util.List;
 
 public class VideoToFrames implements Runnable {
     private final int decodeColorFormat = MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible;
+
+    private static final int COLOR_Format_I420 = 1;
+    private static final int COLOR_Format_NV21 = 2;
+
     private static final String TAG = "VideoToFrames";
     private static final boolean VERBOSE = false;
     private static final long DEFAULT_TIMEOUT_US = 10000;
@@ -168,5 +175,127 @@ public class VideoToFrames implements Runnable {
         for (Callback callback : callbackList) {
             callback.onFinishDecode();
         }
+    }
+
+    private static byte[] imageToYuv(Image image, int colorFormat) {
+        Rect crop = image.getCropRect();
+        int format = image.getFormat();
+        int width = crop.width();
+        int height = crop.height();
+        Image.Plane[] planes = image.getPlanes();
+        byte[] data = new byte[width * height * ImageFormat.getBitsPerPixel(format) / 8];
+        byte[] rowData = new byte[planes[0].getRowStride()];
+        if (VERBOSE) Log.v(TAG, "get data from " + planes.length + " planes");
+        int channelOffset = 0;
+        int outputStride = 1;
+        for (int i = 0; i < planes.length; i++) {
+            switch (i) {
+                case 0:
+                    channelOffset = 0;
+                    outputStride = 1;
+                    break;
+                case 1:
+                    if (colorFormat == COLOR_Format_I420) {
+                        channelOffset = width * height;
+                        outputStride = 1;
+                    } else if (colorFormat == COLOR_Format_NV21) {
+                        channelOffset = width * height + 1;
+                        outputStride = 2;
+                    }
+                    break;
+                case 2:
+                    if (colorFormat == COLOR_Format_I420) {
+                        channelOffset = (int) (width * height * 1.25);
+                        outputStride = 1;
+                    } else if (colorFormat == COLOR_Format_NV21) {
+                        channelOffset = width * height;
+                        outputStride = 2;
+                    }
+                    break;
+            }
+            ByteBuffer buffer = planes[i].getBuffer();
+            int rowStride = planes[i].getRowStride();
+            int pixelStride = planes[i].getPixelStride();
+            if (VERBOSE) {
+                Log.v(TAG, "pixelStride " + pixelStride);
+                Log.v(TAG, "rowStride " + rowStride);
+                Log.v(TAG, "width " + width);
+                Log.v(TAG, "height " + height);
+                Log.v(TAG, "buffer size " + buffer.remaining());
+            }
+            int shift = (i == 0) ? 0 : 1;
+            int w = width >> shift;
+            int h = height >> shift;
+            buffer.position(rowStride * (crop.top >> shift) + pixelStride * (crop.left >> shift));
+            for (int row = 0; row < h; row++) {
+                int length;
+                if (pixelStride == 1 && outputStride == 1) {
+                    length = w;
+                    buffer.get(data, channelOffset, length);
+                    channelOffset += length;
+                } else {
+                    length = (w - 1) * pixelStride + 1;
+                    buffer.get(rowData, 0, length);
+                    for (int col = 0; col < w; col++) {
+                        data[channelOffset] = rowData[col * pixelStride];
+                        channelOffset += outputStride;
+                    }
+                }
+                if (row < h - 1) {
+                    buffer.position(buffer.position() + rowStride - length);
+                }
+            }
+            if (VERBOSE) Log.v(TAG, "Finished reading data from plane " + i);
+        }
+        return data;
+    }
+
+    private static void dumpFile(String filePath, byte[] data) {
+        FileOutputStream outStream;
+        try {
+            outStream = new FileOutputStream(filePath);
+        } catch (IOException ioe) {
+            throw new RuntimeException("Unable to create output file " + filePath, ioe);
+        }
+        try {
+            outStream.write(data);
+            outStream.close();
+        } catch (IOException ioe) {
+            throw new RuntimeException("failed writing data to file " + filePath, ioe);
+        }
+    }
+
+    public void saveI420Frames(final String outputDir) {
+        new File(outputDir).mkdirs();
+        Callback callback = new Callback() {
+            @Override
+            public void onDecodeFrame(int index, Image image) {
+                String outputFilePath = new File(outputDir, String.format("frame_%6d_I420_%dx%d.yuv", index, image.getWidth(), image.getHeight())).getAbsolutePath();
+                dumpFile(outputFilePath, imageToYuv(image, COLOR_Format_I420));
+            }
+
+            @Override
+            public void onFinishDecode() {
+
+            }
+        };
+        addCallback(callback);
+    }
+
+    public void saveNv21Frames(final String outputDir) {
+        new File(outputDir).mkdirs();
+        Callback callback = new Callback() {
+            @Override
+            public void onDecodeFrame(int index, Image image) {
+                String outputFilePath = new File(outputDir, String.format("frame_%6d_NV21_%dx%d.yuv", index, image.getWidth(), image.getHeight())).getAbsolutePath();
+                dumpFile(outputFilePath, imageToYuv(image, COLOR_Format_NV21));
+            }
+
+            @Override
+            public void onFinishDecode() {
+
+            }
+        };
+        addCallback(callback);
     }
 }
