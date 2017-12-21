38832f6c45983ec443bf4bc1d520ee92b1a730fe
Tong ZHAN <zhantong1994@163.com>
2017-12-21 13:05:34 +0800

    使用PermissionsDispatcher处理运行时读写文件权限问题

--- app/src/main/java/com/polarxiong/videoprocess/MainActivity.java
+++ app/src/main/java/com/polarxiong/videoprocess/MainActivity.java
@@ -4,25 +4,23 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.polarxiong.videotoframes.VideoToFrames;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends Activity {
     private static final int REQUEST_CODE_GET_FILE_PATH = 1;
-    private static final int PERMISSION_WRITE_EXTERNAL_STORAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +31,7 @@ public class MainActivity extends Activity {
         buttonFilePathInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_WRITE_EXTERNAL_STORAGE);
-                } else {
-                    getFilePath(REQUEST_CODE_GET_FILE_PATH);
-                }
+                getFilePath(REQUEST_CODE_GET_FILE_PATH);
             }
         });
 
@@ -69,20 +63,6 @@ public class MainActivity extends Activity {
         });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_WRITE_EXTERNAL_STORAGE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getFilePath(REQUEST_CODE_GET_FILE_PATH);
-                } else {
-                    Toast.makeText(this, "需要开启文件读写权限", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -99,7 +79,8 @@ public class MainActivity extends Activity {
         }
     }
 
-    private void getFilePath(int requestCode) {
+    @NeedsPermission({Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE})
+    void getFilePath(int requestCode) {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
